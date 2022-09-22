bf54c6f130b89d00e5b0aafa8226b60b5eabc4a0
Anmol <95535448+AnmolSirola@users.noreply.github.com>
2022-09-23 00:56:39 +0530

    Update Readme.sol

--- Solidity_Advance/Modifier/Readme.sol
+++ Solidity_Advance/Modifier/Readme.sol
@@ -1,1 +1,9 @@
 # Modifiers
+
+Modifiers are code that can be run before and / or after a function call.
+Modifiers can be used to:
+
+- Restrict access
+- Validate inputs
+- Guard against reentrancy hack
+

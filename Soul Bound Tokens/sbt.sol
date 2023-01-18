b8a6cd9e112e45834e4cf25ed4da8c41c2833b97
Anmol <95535448+AnmolSirola@users.noreply.github.com>
2023-01-18 15:43:37 +0530

    sbt editied

---
+++ Soul Bound Tokens/sbt.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract UniversityDegree is ERC721URIStorage {
+    address owner;
+
+    using Counters for Counters.Counter;
+    Counters.Counter private tokenIds;
+
+    constructor() ERC721("UniversityDegree" , "Degree"){
+        owner = msg.sender;
+    }    
+
+    mapping(address => bool) public issuedDegree;
+
+    modifier onlyOwner(){
+        require(msg.sender==owner);
+        _;
+    }
+
+    function issuedDegree(address to) external onlyOwner{
+        issuedDegree[to]= true;
+    }
+    
+    function claimDegree(string memory tokenURI) public returns(uint256){
+        require(issuedDegree[msg.sender], "Degree is not issued");
+
+        tokenIds.increment();
+        uint256 newItemId = tokenIds.current();
+        _mint(msg.sender ,newItemId);
+        _setTokenURI(newItemId , tokenURI);
+
+        personToDegree[msg.sender] = tokenURI;
+        issuedDegree[msg.sender] = false ;
+
+        return newItemId;
+      
+    }
+
+    mapping(address => string) public personToDegree;
+
+    bool cannotTransfer = true;
+
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId) public virtual override {
+        require(cannotTransfer == false, "You cannot tranfer this NFT.") ;   
+    } 
+
+    function tranferFrom(address _from, address _to, uint256 _tokenId) public virtual override {
+        require(cannotTransfer == false, "You cannot transfer this NFT.");
+    }
+    
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes memory _data) public virtual override{
+        require(cannotTransfer == false, "You cannot transfer this NFT");
+    }
+
+}
